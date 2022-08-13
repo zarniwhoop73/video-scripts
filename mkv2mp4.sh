7cbd9a0a990f2801ff08e780ddce80eae58acc2e
zarniwhoop <zarniwhoop@ntlworld.com>
2022-08-13 05:29:13 +0100

    mkv2mp4.sh - try to handle all mkv formats that I have.

--- mkv2mp4.sh
+++ mkv2mp4.sh
@@ -10,14 +10,27 @@
 # Mine are all Matroska, with video format AVC and audio format AAC LC
 # The various mp4 siles I have use AVC video format but audio formats
 # of AVC LC, AVC LOC SBR, AAC LC, or MPEG Audio.
-# this implies that my AAC LC audio, and AVC video can just be copied.	
+# this implies that my AAC LC audio, and AVC video can just be copied.
+# Later experimentation shows that some mkv files have other audio codecs
+# which tend not to work, giving a silent result unless recoded.
 #
 # This uses ffmpeg (tested with ffmpeg-5.1), mediainfo (the commandline
-# version, tested with 21.09) and a standard LFS build.
+# version, tested with 21.09) and a standard LFS build.  If the audio
+# format needs to be recoded (I found one mkv with ogg audio, which
+# resulted in silent ouput without recoding) I assume that ffmpeg has
+# been built with libfdk_aac (a nont redistributable build).  If you
+# do not have that, I suppose AAC="-c:a aac -b:a 192k" from the ffmpeg
+# wiki will do an adequate job.
 #
 # Usage:
 # supply a filename,mkv - this should be an mkv file with AVC video and
 # AAC LC audio. The output will be filename.mp4
+#
+
+# if needing to record (see above)
+FREQ=16000
+AAC="-acodec libfdk_aac -cutoff $FREQ -b:a 96k"
+
 
 usage() {
 	echo "$0: convert an mkv (Matroska) file,"
@@ -45,7 +58,7 @@ else
 	echo "confirming $1 is a Matroska file"
 	file $1 | grep 'Matroska data'
 	if [ $? -ne 0 ]; then
-		echo "$1 is not a Matroska file"
+		echo "ERROR: $1 is not a Matroska file"
 		xit 1
 	fi
 	echo "confirming mediainfo is installed"
@@ -60,6 +73,24 @@ else
 	AUDIO=$(mediainfo $1 | grep -E '^Format *:' | cut -d ':' -f2 | sed 's/ //' |
 	 head -n 3 | tail -n1)
 	#echo "formats are >$VIDEO< and >$AUDIO<"
+	# FIXME - forgot to test these formats!			
+	if [ "$VIDEO" != "AVC" ]; then
+		echo "WARNING: $1 is $VIDEO not Advanced Video Codec"
+		RECODE=true
+		sleep 3
+	fi
+	if [ "$AUDIO" = "AAC LC" ] || [ "$AUDIO" = "AVC LC" ] | [ "$AUDIO" = "ACV LC SBR" ]; then
+		SOUND=ok
+	elif [ "$AUDIO" = "MPEG Audio" ]; then
+		SOUND=maybe
+		echo "Uncertain if sound will work, please review result"
+		sleep 2
+	else
+		echo "Audio format $AUDIO is not compatible with an mp4 file"
+		echo "Will need to recode it"
+		SOUND=recode
+		sleep 2
+	fi
 
 fi
 
@@ -67,6 +98,27 @@ fi
 BASENAME=$(basename $1 .mkv)
 OUTFILE="$BASENAME.mp4"
 
+# sort out what to do
 echo "creating $OUTFILE from $1"
+if [ "$RECODE" = "true" ]; then
+	# assume default crf 23 will be good enough
+	# for both -c:v and -vcodec the - is removed
+	# using --vcodec gives Unrecognized option '-vcodec libx264'
+	# if I hard-code -c:v and pass libx264 I get
+	# Unable to find a suitable output format for 'libx264'
+	# So, try hardcoding the parts
+	if [ "$SOUND" != "recode" ]; then
+		ffmpeg -i $1 -c:v libx264 -c:a copy $OUTFILE
+	else
+		ffmpeg -i $1 -c:v libx264 $AAC $OUTFILE
+	fi
+else
+	if [ "$SOUND" != "recode" ]; then
+		ffmpeg -i $1 -c:v copy -c:a copy $OUTFILE
+	else
+		ffmpeg -i $1 -c:v copy $AAC $OUTFILE
+	fi
+fi
 
-ffmpeg -i $1 -c:v copy -c:a copy $OUTFILE
+sleep 5
+ffmpeg -i $1 $FFV $FFA $OUTFILE
